7c64fd83047ad1f4e541975408306a97dc12bb2b
Matteo <mboffadossi@marvell.com>
2023-09-05 14:40:32 +0200

    [MB] Platform initial release

---
+++ main.py
@@ -0,0 +1,42 @@
+import pygame
+import sys
+
+from settings import *
+
+
+class Game:
+    def __init__(self):
+        ppass, pfail = pygame.init()
+        self.screen = pygame.display.set_mode(RES)
+        self.timer = pygame.time.Clock()
+
+        self.delta_time = 0
+
+    def draw(self):
+        # Draw new frame
+        self.screen.fill('black')
+        pygame.display.flip()
+        self.delta_time = self.timer.tick(TICK)
+        msg1 = f'{self.clock.get_fps():.1f}'
+        pygame.display.set_caption(f'FPS: {msg1}')
+
+    def update(self):
+        # Execute phisics and state updates
+        pass
+
+    def run(self):
+        while True:
+            self.check_events()
+            self.update()
+            self.draw()
+
+    def check_events(self):
+        for event in pygame.event.get():
+            if (event.type == pygame.QUIT) or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+                pygame.quit()
+                sys.exit(0)
+
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
---
+++ settings.py
@@ -0,0 +1,14 @@
+TOL = 1e-6
+
+RES = WIDTH, HEIGHT = 1200, 900
+FPS = 60
+TICK = 60
+
+MAP_TileX = 60
+MAP_TileY = 60
+
+MAP_Cols = WIDTH / MAP_TileX
+MAP_Rows = HEIGHT / MAP_TileY
+
+# MAP_StepX = WIDTH / MAP_TileX
+# MAP_stepY = HEIGHT / MAP_TileY
